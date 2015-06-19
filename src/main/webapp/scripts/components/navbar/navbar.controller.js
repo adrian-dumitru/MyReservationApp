106a20156898a3ee9fe2fe6e80932c1c8d5edb3e
Dumitru Adrian <dumitru.adrian92@gmail.com>
2015-06-19 21:04:46 +0300

    Logout facebook

--- src/main/webapp/scripts/components/navbar/navbar.controller.js
+++ src/main/webapp/scripts/components/navbar/navbar.controller.js
@@ -1,11 +1,17 @@
 'use strict';
 
 angular.module('myreservationappApp')
-    .controller('NavbarController', function ($scope, $location, $state, Auth, Principal) {
+    .controller('NavbarController', function ($scope, $location, $state, Auth, Principal,Facebook) {
         $scope.isAuthenticated = Principal.isAuthenticated;
         $scope.$state = $state;
 
         $scope.logout = function () {
+
+            Facebook.logout(function() {
+                $scope.$apply(function() {
+                });
+            });
+
             Auth.logout();
             $state.go('home');
         };
